c876ab80bd6a0814a61b57dbb7c396e62fb0976b
Binshad <Binshadn1@gmail.com>
2020-03-11 22:38:41 +0530

    bin test

--- INVENTORY.UI/Program.cs
+++ INVENTORY.UI/Program.cs
@@ -17,7 +17,7 @@ namespace INVENTORY.UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FSplash());
-            //test 10.03.2020--binshad dsdf
+            //test 11.03.2020--binshad
         }
     }
 }
